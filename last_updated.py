32dd4a667ab1eba514df1bcb2d73f2fe1c438795
Kanwar Hamza Shuja <kanwarhamza.shuja@unito.it>
2025-05-07 23:55:46 +0200

    Update last_updated.py
    changes again

--- last_updated.py
+++ last_updated.py
@@ -1613,8 +1613,8 @@ def main():
 
     st.title("📚 A fully Automated Systematic Review Application")
     st.write("Developed by **Kanwar Hamza Shuja**")
-    st.write(Email: kanwarhamzashuja.gmail.com; kanwarhamza.shuja@unito.it)
-    st.write(https://github.com/KanwarHamza)
+    st.write("Email: kanwarhamzashuja.gmail.com; kanwarhamza.shuja@unito.it")
+    st.write("https://github.com/KanwarHamza")
     st.write("7/05/2025")
 
     st.markdown("""
